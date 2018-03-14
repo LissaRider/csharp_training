6ce06cc0a7e4bd6bb285379d657738f4a2cafdd0
vsmirnova <vsmirnova@VSMIRNOVA>
2018-03-14 15:41:00 +0300

    Реализован тест для добавления групп для адресной книги с графическим интерфейсом Windows: с использованием White.

---
+++ addressbook_tests_white/addressbook_tests_white/appmanager/GroupHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestStack.White;
+using TestStack.White.InputDevices;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.TreeItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.WindowsAPI;
+using System.Windows.Automation;
+
+namespace addressbook_tests_white
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public static string DELETEGROUPWINTITLE = "Delete group";
+
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public void Add(GroupData newGroup)
+        {
+            Window dialogue = OpenGroupsDialogue();
+
+            InitGroupAdding(dialogue);
+            TextBox textBox = (TextBox)dialogue.Get(SearchCriteria.ByControlType(ControlType.Edit));
+            textBox.Enter(newGroup.Name);
+            Keyboard.Instance.PressSpecialKey(KeyboardInput.SpecialKeys.RETURN);
+
+            CloseGroupsDialogue(dialogue);
+        }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            Window dialogue = OpenGroupsDialogue();
+            Tree tree = dialogue.Get<Tree>("uxAddressTreeView");
+            TreeNode root = tree.Nodes[0];
+            foreach (TreeNode item in root.Nodes)
+            {
+                list.Add(new GroupData()
+                {
+                    Name = item.Text
+                });
+            }
+            CloseGroupsDialogue(dialogue);
+            return list;
+        }
+
+        // Common methods   
+        public Window OpenGroupsDialogue()
+        {
+            manager.MainWindow.Get<Button>("groupButton").Click();
+            return manager.MainWindow.ModalWindow(GROUPWINTITLE);
+        }
+
+        public void CloseGroupsDialogue(Window dialogue)
+        {
+            dialogue.Get<Button>("uxCloseAddressButton").Click();
+        }
+
+        // Group creation methods
+        public void InitGroupAdding(Window dialogue)
+        {
+            dialogue.Get<Button>("uxNewAddressButton").Click();
+        }
+    }
+}
+
---
+++ addressbook_tests_white/addressbook_tests_white/appmanager/HelperBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+
+namespace addressbook_tests_white
+{
+    public class HelperBase
+    {
+        protected ApplicationManager manager;
+
+        public HelperBase(ApplicationManager manager)
+        {
+            this.manager = manager;
+        }
+    }
+}
